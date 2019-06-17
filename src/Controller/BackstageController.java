a0e4ed46ae7b1a31f9e6fa91cbb4132ac46d947d
tanghao <850528794@qq.com>
2019-06-17 10:25:49 +0800

    first-test

---
+++ src/Controller/BackstageController.java
@@ -0,0 +1,17 @@
+package Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/manager")
+public class BackstageController {
+	
+	@RequestMapping("/index")
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("/index");
+		return mv;
+	}
+	
+}
